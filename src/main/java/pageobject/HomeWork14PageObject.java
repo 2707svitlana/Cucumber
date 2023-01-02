b79c1036b9fe03eb6a5ba40970b9ba335a1a14c0
админ <2707svitlana@gmail.com>
2023-01-02 20:12:06 +0200

    HomeWork14

---
+++ src/main/java/pageobject/HomeWork14PageObject.java
@@ -0,0 +1,34 @@
+package pageobject;
+
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class HomeWork14PageObject {
+
+
+    @FindBy(xpath = "//input[@name=\"certificate\"]")
+    WebElement certificateCheckField;
+
+    @FindBy(xpath = "//p[@class=\"certificate-check_message\"]")
+    WebElement message;
+
+    public void sendKeysToCheckForm(String str) {
+        certificateCheckField.sendKeys(str);
+        certificateCheckField.sendKeys(Keys.ENTER);
+    }
+
+    public String getMessage() {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+
+        }
+        return message.getText();
+    }
+
+
+    }
+
+
